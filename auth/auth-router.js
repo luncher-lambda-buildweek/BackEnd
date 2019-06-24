644b36480266561267fa99fe950a1ac30d7268d6
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-24 11:30:40 -0700

    added POST register and login routes

---
+++ auth/auth-router.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken')
+
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10); 
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            const token = generateToken(user)
+            res.status(201).json({token});
+                
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post('/login', (req, res) => {
+    let { email, password } = req.body;
+
+    Users.findBy({ email })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({
+                    message: `Welcome ${user.email}!`,
+                    token
+                });
+            } else {
+                res.status(401).json({ message: 'Incorrect email or password' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id, 
+        email: user.email,
+        roles: user.role
+    };
+
+    const options = {
+        expiresIn: '1d',
+
+    }
+
+
+    return jwt.sign(payload, secrets.jwtSecret, options)
+
+}
+
+module.exports = router;
