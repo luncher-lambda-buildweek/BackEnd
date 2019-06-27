92aa085ecfb8be87359cea367022fc6a67deac0b
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-27 13:39:49 -0700

    testing to see if heroku error is fixed

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,10 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-const db = knex(knexConfig.development)
+// const db = knex(knexConfig.development)
 
-module.exports = db;
+// module.exports = db;
+
+const db = process.env.DB_ENV || 'development';
+
+module.exports = knex(knexConfig[db]);
